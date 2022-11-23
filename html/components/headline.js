a0d07bc811fe2a99ad6e18061367139be3fdc741
Mwni <manuel@tigmar.de>
2022-11-23 14:47:43 +0100

    add headline tier support

--- html/components/headline.js
+++ html/components/headline.js
@@ -1,6 +1,12 @@
 import { Fragment } from '@architekt/render'
 import { Element } from '../dom.js'
 
-export default Fragment(({ text }) => {
-	Element('h1', {class: 'headline'}, text)
+export default Fragment(({ text, tier, class: cls }) => {
+	Element(
+		`h${tier || 1}`, 
+		{
+			class: ['headline', cls]
+		}, 
+		text
+	)
 })
