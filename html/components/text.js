334093555dd1384f6330b4d030570dc632ba41aa
Mwni <manuel@tigmar.de>
2022-11-25 11:41:06 +0100

    add text component class support

--- html/components/text.js
+++ html/components/text.js
@@ -1,6 +1,12 @@
 import { Fragment } from '@architekt/render'
 import { Element } from '../dom.js'
 
-export default Fragment(({ text }) => {
-	Element('span', {class: 'text'}, text)
+export default Fragment(({ text, ...props }) => {
+	Element(
+		'span', 
+		{
+			class: ['text', props.class]
+		}, 
+		text
+	)
 })
