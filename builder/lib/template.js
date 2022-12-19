8f198b03eae3071d2de1c4cb8a9fc91721a37a73
Mwni <manuel@tigmar.de>
2022-12-19 04:54:18 +0100

    drop old code

--- builder/lib/template.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'
-import Handlebars from 'handlebars'
-
-export function template({file, fields}){
-	return Handlebars.compile(fs.readFileSync(file, 'utf-8'))(fields)
-}
--- builder/transforms/index.js
+++ builder/transforms/index.js
@@ -1,7 +1,5 @@
-import * as api from './api.js'
 import * as asyncImports from './imports.js'
 
 export default [
-    api,
     asyncImports
 ]
