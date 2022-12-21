cfd260335e9ebf2f8c789f8ffcf20eadba19a002
Mwni <manuel@tigmar.de>
2022-12-21 15:39:07 +0100

    add fragment patching support

--- render/fragment.js
+++ render/fragment.js
@@ -1,4 +1,8 @@
+import { ctx } from './context.js'
+
 export default render => {
+	let childPatch
+
 	return (props, content) => {
 		if(!props){
 			props = {}
@@ -7,6 +11,17 @@ export default render => {
 			props = {}
 		}
 
+		if(ctx.node.childPatch){
+			childPatch = ctx.node.childPatch;
+
+			[render, props, content] = childPatch(render, props, content)
+
+			ctx.node.childPatch = undefined
+		}
+
 		render(props, content)
+
+		if(childPatch)
+			ctx.node.childPatch = childPatch
 	}
 }
