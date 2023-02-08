c7fc964ce8bc69cd7a2e9de72a93187cac468b0f
Mwni <manuel@tigmar.de>
2023-02-08 06:22:12 +0100

    move tailwind plugin state into init

--- tailwind/index.js
+++ tailwind/index.js
@@ -5,72 +5,75 @@ import tailwindResolveConf from 'tailwindcss/lib/public/resolve-config.js'
 const resolveConfig = tailwindResolveConf.default
 
 
-let state = {
-	context: null,
-	watcher: null,
-	changedContent: [],
-	configDependencies: new Set(),
-	contextDependencies: new Set(),
-	contentPaths: [],
-	get config() {
-		return this.context.tailwindConfig
-	},
-	getContext({ createContext, configPath, config, nonCssContent }) {
-		if (this.context) {
-			this.context.changedContent = this.changedContent.splice(0)
-
-			return this.context
-		}
-
-	  	this.context = createContext(
-			resolveConfig(
-				config, 
-				{
-					content: { 
-						files: []
-					}
-				}
-			), 
-			[]
-		)
-
-		Object.assign(this.context, {
-			userConfigPath: configPath,
-		})
-
-		this.context.changedContent.push(...nonCssContent)
-  
-	 	return this.context
-	}
-}
 
 export default config => ({
 	id: 'tailwind',
 	postcssPlugins: [
 		{
-			init: ({ rootPath, chunk }) => ({
-				postcssPlugin: 'tailwindcss',
-				async Once(root, { result }){
-					let configPath = `${rootPath}/tailwind.config.js`
-					let config = fs.existsSync(configPath)
-						? (await import(`file://${configPath}`)).default
-						: {}
-
-					tailwind.default(({ createContext }) => {
-						return () => state.getContext({
-							createContext,
-							configPath,
-							config,
-							root,
-							result,
-							nonCssContent: [{
-								content: chunk.code,
-								extension: 'js'
-							}]
+			init: ({ rootPath, chunk }) => {
+				let state = {
+					context: null,
+					watcher: null,
+					changedContent: [],
+					configDependencies: new Set(),
+					contextDependencies: new Set(),
+					contentPaths: [],
+					get config() {
+						return this.context.tailwindConfig
+					},
+					getContext({ createContext, configPath, config, nonCssContent }) {
+						if (this.context) {
+							this.context.changedContent = this.changedContent.splice(0)
+				
+							return this.context
+						}
+				
+						  this.context = createContext(
+							resolveConfig(
+								config, 
+								{
+									content: { 
+										files: []
+									}
+								}
+							), 
+							[]
+						)
+				
+						Object.assign(this.context, {
+							userConfigPath: configPath,
 						})
-					})(root, result)
+				
+						this.context.changedContent.push(...nonCssContent)
+				  
+						 return this.context
+					}
 				}
-			})
+				
+				return {
+					postcssPlugin: 'tailwindcss',
+					async Once(root, { result }){
+						let configPath = `${rootPath}/tailwind.config.js`
+						let config = fs.existsSync(configPath)
+							? (await import(`file://${configPath}`)).default
+							: {}
+
+						tailwind.default(({ createContext }) => {
+							return () => state.getContext({
+								createContext,
+								configPath,
+								config,
+								root,
+								result,
+								nonCssContent: [{
+									content: chunk.code,
+									extension: 'js'
+								}]
+							})
+						})(root, result)
+					}
+				}
+			}
 		}
 	],
 })
