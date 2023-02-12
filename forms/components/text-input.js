60ca5025a69c08a632412c1d3e7f93b193c0e6d2
Mwni <manuel@tigmar.de>
2023-02-12 07:56:56 +0100

    rename to text input

--- forms/components/text-input.js
+++ forms/components/text-input.js
@@ -1,10 +1,11 @@
 import { Fragment, TextInput } from '@architekt/ui'
 
-export default Fragment(({ model, key, ...props }) => {
+export default Fragment(({ model, key, disabled, ...props }) => {
 	TextInput({
 		...props,
 		text: model.get(key),
 		onInput: event => model.set(key, event.target.value),
-		invalid: !!model.status[key]?.invalid
+		invalid: !!model.fieldStatus[key]?.invalid,
+		disabled: model.submitting || disabled
 	})
 })
--- forms/index.js
+++ forms/index.js
@@ -1,4 +1,4 @@
 export { default as createModel } from './model.js'
 export { default as Select } from './components/select.js'
-export { default as Text } from './components/text.js'
+export { default as TextInput } from './components/text-input.js'
 export { default as Status } from './components/status.js'
