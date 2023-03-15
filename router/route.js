73d3b39388050eed0eaf6ecc6c0fcd7f4df2860d
Mwni <manuel@tigmar.de>
2023-03-16 00:59:03 +0100

    make route component virtual

--- router/route.js
+++ router/route.js
@@ -1,18 +1,15 @@
 import { ctx } from '@architekt/render'
-import { Fragment, Component } from '@architekt/ui'
+import { Fragment } from '@architekt/ui'
 
-
-export default Fragment(({ path, fallback, bad }, content) => {
+export default ({ path, fallback, bad }, content) => {
 	let route = ctx.downstream.route.maybeEnter({ path, fallback, bad })
 
 	if(route){
 		Holder({ route }, content)
 	}
-})
+}
 
-const Holder = Component(() => {
-	return ({ route }, content) => {
-		ctx.downstream.route = route
-		content()
-	}
+const Holder = Fragment(({ route }, content) => {
+	ctx.downstream.route = route
+	content(route.params)
 })
