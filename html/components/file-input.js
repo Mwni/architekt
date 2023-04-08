f04fe275113bc97af8ea1d9ecb2088ac6e436e02
Mwni <manuel@tigmar.de>
2023-04-08 19:11:24 +0200

    dispatch drop activity on file input

--- html/components/file-input.js
+++ html/components/file-input.js
@@ -1,10 +1,13 @@
 import { Fragment } from '@architekt/render'
+import { getContext } from '@architekt/ui'
 import { Element } from '../dom.js'
 
 export default Fragment(({ onChange, multiple, ...props }, content) => {
+	let { downstream, redraw } = getContext()
 	let xid = Math.random()
 		.toString(32)
 		.slice(2, 10)
+
 
 	if(content){
 		Element(
@@ -14,12 +17,24 @@ export default Fragment(({ onChange, multiple, ...props }, content) => {
 				for: xid,
 				ondragover: event => {
 					event.preventDefault()
+					downstream.dropActive = true
+					redraw()
 				},
 				ondragenter: event => {
 					event.preventDefault()
+					downstream.dropActive = true
+					redraw()
+				},
+				ondragleave: event => {
+					event.preventDefault()
+					downstream.dropActive = false
+					redraw()
 				},
 				ondrop: event => {
 					let input = document.getElementById(xid)
+
+					downstream.dropActive = false
+					redraw()
 
 					event.preventDefault()
 					input.files = event.dataTransfer.files
