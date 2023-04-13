3b8b4864b13104b7020e8012674a05c44a989fb4
Mwni <manuel@tigmar.de>
2023-04-13 03:51:17 +0200

    add text input multiline support

--- html/components/text-input.js
+++ html/components/text-input.js
@@ -1,16 +1,29 @@
 import { Fragment } from '@architekt/render'
 import Element from '../element.js'
 
-export default Fragment(({ text, onInput, onChange, ...props }) => {
-	Element(
-		{
-			tag: 'input',
-			type: 'text',
-			...props,
-			class: ['a-textinput', props.class],
-			value: text,
-			oninput: onInput,
-			onchange: onChange
-		}
-	)
+export default Fragment(({ text, multiline, onInput, onChange, ...props }) => {
+	if(multiline){
+		return Element(
+			{
+				tag: 'textarea',
+				...props,
+				class: ['a-textinput', 'multiline', props.class],
+				value: text,
+				oninput: onInput,
+				onchange: onChange
+			}
+		)
+	}else{
+		return Element(
+			{
+				tag: 'input',
+				type: 'text',
+				...props,
+				class: ['a-textinput', props.class],
+				value: text,
+				oninput: onInput,
+				onchange: onChange
+			}
+		)
+	}
 })
