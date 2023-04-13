8f8b957578fb9c8d8607eb7dee60b1d3da527c36
Mwni <manuel@tigmar.de>
2023-04-14 01:08:56 +0200

    reapply interactive listeners per render

--- html/components/interactive.js
+++ html/components/interactive.js
@@ -1,14 +1,16 @@
 import { Component } from '@architekt/ui'
 
 export default Component(({ ctx, onTap }) => {
-	ctx.afterRender(
-		() => {
-			for(let element of ctx.dom){
-				element.classList.add('a-interactive')
-				element.addEventListener('click', onTap)
-			}
-		}
-	)
+	return (_, content) => {
+		content()
 
-	return (_, content) => content()
+		ctx.afterRender(
+			() => {
+				for(let element of ctx.dom){
+					element.classList.add('a-interactive')
+					element.addEventListener('click', onTap)
+				}
+			}
+		)
+	}
 })
