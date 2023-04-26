d92b12043497f241108123ad4a83177d53257a93
Mwni <manuel@tigmar.de>
2023-04-27 01:56:41 +0200

    improve fetch network error message

--- api/fetch.js
+++ api/fetch.js
@@ -6,6 +6,12 @@ function architektFetch(url, options){
 	let combinedUrl = config.urlBase + url
 
 	return fetch(combinedUrl, options)
+		.catch(error => {
+			if(error.message === 'Failed to fetch')
+				throw new Error('Network Error')
+			else
+				throw error
+		})
 }
 
 architektFetch.config = newConfig => {
