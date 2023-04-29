d66e3c24019f1cf9ae91b74127ab67873d974976
Mwni <manuel@tigmar.de>
2023-04-29 07:38:44 +0200

    add auto focus feature for text inputs

--- html/components/text-input.js
+++ html/components/text-input.js
@@ -1,29 +1,41 @@
-import { Fragment } from '@architekt/render'
+import { Component } from '@architekt/render'
 import Element from '../element.js'
 
-export default Fragment(({ text, multiline, secure, onInput, onChange, ...props }) => {
-	if(multiline){
-		return Element(
-			{
-				tag: 'textarea',
-				...props,
-				class: ['a-textinput', 'multiline', props.class],
-				value: text,
-				oninput: onInput,
-				onchange: onChange
-			}
-		)
-	}else{
-		return Element(
-			{
-				tag: 'input',
-				type: secure ? 'password' : 'text',
-				...props,
-				class: ['a-textinput', props.class],
-				value: text,
-				oninput: onInput,
-				onchange: onChange
-			}
-		)
+export default Component(({ ctx, autoFocus }) => {
+	if(autoFocus){
+		ctx.afterRender(() => {
+			ctx.dom[0].focus()
+		})
+	}
+
+	return ({ text, multiline, secure, maxLength, onKeyDown, onInput, onChange, ...props }) => {
+		if(multiline){
+			return Element(
+				{
+					tag: 'textarea',
+					...props,
+					class: ['a-textinput', 'multiline', props.class],
+					value: text,
+					maxlength: maxLength,
+					onkeydown: onKeyDown ? e => (onKeyDown(e), true) : undefined,
+					oninput: onInput,
+					onchange: onChange
+				}
+			)
+		}else{
+			return Element(
+				{
+					tag: 'input',
+					type: secure ? 'password' : 'text',
+					...props,
+					class: ['a-textinput', props.class],
+					value: text,
+					maxlength: maxLength,
+					onkeydown: onKeyDown ? e => (onKeyDown(e), true) : undefined,
+					oninput: onInput,
+					onchange: onChange
+				}
+			)
+		}
 	}
 })
